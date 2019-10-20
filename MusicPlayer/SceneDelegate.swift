4358adbad5795be95c2ab74276e1f775ac6151db
Hayk Mnatsakanyan <Hayk.Mnatsakanyan@penske.com>
2019-10-20 16:04:40 +0400

    updated configured script for swiftlint

--- MusicPlayer/SceneDelegate.swift
+++ MusicPlayer/SceneDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
@@ -16,6 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        var a = 5
+        a = 30
+        print(a)
     }
 
     func sceneDidDisconnect(_: UIScene) {
